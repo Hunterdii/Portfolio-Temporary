69d138f4de3a8ab8fb3ad38112281e61b2b215ab
Patel HetKumar SandipBhai <124852522+Hunterdii@users.noreply.github.com>
2024-01-29 00:29:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+/* ========================== typing animation ========================= */
+var typed = new Typed(".typing",{
+    strings:["","Frontend developer","web Designer","Graphic Designer","Enthusiast"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
